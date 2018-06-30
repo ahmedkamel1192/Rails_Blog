1fac827e991fa03160eb0247c6cb1199b8da13f2
Minos32x <minasamy65@gmail.com>
2018-06-30 20:36:18 +0200

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :articles do 
      member do
-        get 'add_favourite'
-        get 'remove_favourite'
+        put 'add_favourite'
+        delete 'remove_favourite'
      end  
    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -10,15 +10,14 @@ Rails.application.routes.draw do
   post "signup" => "signup#create"
   get "signin" => "session#new"
   post "signin" => "session#create"
-  get "signout" => "session#destroy"
+  delete "signout" => "session#destroy"
   get "favourite_articles" => "user#my_favourite"
   get "all_user" => "user#index"
-  get "follow" => "user#follow"
-  get "unfollow" => "user#unfollow"
+  put "follow" => "user#follow"
+  delete "unfollow" => "user#unfollow"
   get "followees_articles" => "user#my_followees_articles"
   get "my_followers_list" => "user#my_followers_list"
   get "my_followed_list" => "user#my_followed_list"
-
 
   root to: 'articles#index'
 end
